311bc2d568918d4d4127af6c0dea85937e919b32
Elton <eltonp3103@gmail.com>
2020-02-01 14:56:39 -0500

    responsiveness done a little

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ const clogStyle = `
 const clogMsg = "👋 konnichiwa";
 
 console.log(`%c ${clogMsg}`, clogStyle);
+
+const scrollIndicator = document.getElementById("scroll-indicator");
+const navBar = document.getElementById("nav");
+const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+document.addEventListener("scroll", () => {
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    if (winScroll > 0.5 * vh && winScroll < 6.35 * vh) {
+        navBar.style.color = "var(--text-dark-bg)";
+    } else {
+        navBar.style.color = "var(--text-light-bg)";
+    }
+    const scrolled = (winScroll / height);
+    scrollIndicator.value = scrolled;
+}) 
