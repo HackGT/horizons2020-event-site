7cefad25fa7aee35c4c44cafe8709e2c19b4796c
Elton <eltonp3103@gmail.com>
2020-02-02 10:14:55 -0500

    added footer; added deployment files

--- index.js
+++ index.js
@@ -9,16 +9,49 @@ console.log(`%c ${clogMsg}`, clogStyle);
 
 const scrollIndicator = document.getElementById("scroll-indicator");
 const navBar = document.getElementById("nav");
-const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
 document.addEventListener("scroll", () => {
+    const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
     const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    if (winScroll > 0.5 * vh && winScroll < 6.35 * vh) {
+    if (winScroll > 0.5 * vh && winScroll < 6.35 * vh
+        || winScroll > 10 * vh) {
         navBar.style.color = "var(--text-dark-bg)";
     } else {
         navBar.style.color = "var(--text-light-bg)";
     }
     const scrolled = (winScroll / height);
     scrollIndicator.value = scrolled;
-}) 
+});
+
+window.addEventListener("resize", function() {
+	lax.updateElements()
+});
+
+const emojis = [
+    '😖',
+    '😞',
+    '😵',
+    '😠',
+    '❤',
+    '💔',
+    '💓',
+    '💕',
+    '💡',
+    '💢',
+    '👊',
+    '🎵',
+    '💋',
+    '✨',
+    '✌️',
+    '✋',
+    '🎤',
+    '🎥',
+    '🎧',
+    '🎨',
+];
+
+const ey = document.getElementById("ey");
+setInterval(() => {
+    ey.innerHTML = emojis[Math.floor((Math.random() * emojis.length))];
+}, 1000);
