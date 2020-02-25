ff0b036c2c1f52c93a37043d4db26029f4d6e754
Elton <eltonp3103@gmail.com>
2020-02-25 16:27:42 -0500

    sidebar bug fix

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ document.addEventListener("scroll", () => {
     const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     if (winScroll > 0.5 * vh && winScroll < 6.35 * vh
-        || winScroll > 9 * vh) {
+        || winScroll > 10 * vh) {
             navBar.forEach(e => {
                 e.style.color = "var(--text-dark-bg)";
             });
